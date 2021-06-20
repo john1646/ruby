412d270ff55391e1e6c2a4b28048bb361717742a
John Mario Gonzalez Correa <john1646@hotmail.com>
2021-06-19 21:18:43 -0500

    segunda clase

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 get "/bienvenida",to: "home#index"
 
+get "articles/new", to: "articles#new"
+post "articles", to: "articles#create"
 end
